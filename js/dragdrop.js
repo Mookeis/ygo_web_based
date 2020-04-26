bead90f3b72b19ea5920d20535995b4f65be5eb4
Mackenzie Dang <39176896+Mookeis@users.noreply.github.com>
2020-04-26 01:08:08 -0400

    Revert "Heroku implementation"
    This reverts commit e98e946c8d03edac58244b6d590b55bdf934cf8c.

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -12,4 +12,11 @@ function drop(dropevent) {
     dropevent.preventDefault();
     let data = dropevent.dataTransfer.getData("text");
     socket.emit('drop event', dropevent.target.getAttribute('id'), data);
+    socket.on('drop event', function(target, d) {
+        if (document.getElementById(target).getAttribute('data-appendto') === "true") {
+            document.getElementById(target).appendChild(document.getElementById(d));
+        } else {
+            document.getElementById(target).parentNode.appendChild(document.getElementById(d));
+        }
+    });
 }

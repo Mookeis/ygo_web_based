8660d513b0714ce1dc0ec776c8785584c46ad254
Mackenzie Dang <39176896+Mookeis@users.noreply.github.com>
2020-04-28 02:09:26 -0400

    Update DB connection Info

--- signup.php
+++ signup.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_HOST = 'us-cdbr-iron-east-01.cleardb.net';
+$DATABASE_USER = 'b1c6aa7e68c2a0';
+$DATABASE_PASS = 'a248a5e5';
+$DATABASE_NAME = 'heroku_225f13460926939';
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
